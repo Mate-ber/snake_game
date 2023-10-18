07a8059fba66f6bd889ec9dcc870d3ccaf2eb372
Mate-ber <72556058+Mate-ber@users.noreply.github.com>
2023-10-18 09:53:01 +0700

    Update Snake_Score.py

--- Snake_Score.py
+++ Snake_Score.py
@@ -1,31 +1,54 @@
 import turtle
 
+file_name = "Highest_score_snake.txt"
+
 
 class Score:
     def __init__(self):
-        f = open("Highest_score_snake.txt", "r")
-        self.high_score = int(f.read())
-        f.close()
+        self.create_score()
+        self.high_score = self.read_score()
         self.score = 0
         self.title = turtle.Turtle()
         self.title.color("white")
         self.title.speed(0)
         self.title.hideturtle()
         self.title.goto(0, 260)
-        self.update_score()
+        self.update_title()
 
-    def update_score(self):
+    def update_title(self):
         self.title.clear()
         self.title.write(f"Score: {self.score} Highest Score {self.high_score}", align="center", font=("Courier", 24, "normal"))
 
     def increase_score(self):
         self.score += 1
-        self.update_score()
+        self.update_title()
 
-    def reset_score(self):
+    def reset_title(self):
         if self.high_score < self.score:
-            with open("Highest_score_snake.txt", "w") as f:
-                f.write(str(self.score))
-                self.high_score = self.score
+            self.change_score()
         self.score = 0
-        self.update_score()
+        self.update_title()
+
+    def read_score(self):
+        f = open(file_name, "r")
+        num = int(f.read())
+        f.close()
+        return num
+
+    def change_score(self):
+        with open(file_name, "w") as f:
+            f.write(str(self.score))
+            self.high_score = self.score
+
+    def create_score(self):
+        if not self.check_exists():
+            f = open(file_name, "x")
+            f.write("0")
+            f.close()
+
+    def check_exists(self):
+        try:
+            with open(file_name, "r"):
+                return True
+        except FileNotFoundError:
+            return False
